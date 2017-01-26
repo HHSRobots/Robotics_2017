67ba4763fab147854612b04d4d9cad44b6a7aec1
18gishc <18gishc@25.26.54.176>
2017-01-25 20:29:18 -0500

    Hey

--- src/org/usfirst/frc/team554/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team554/robot/subsystems/Shooter.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class Shooter extends Subsystem {
-	//hey
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	
